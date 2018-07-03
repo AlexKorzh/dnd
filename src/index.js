5344ecfad1987ff9a995195cafc22498c1be27e0
Alex <korzhlesha@gmail.com>
2018-07-03 13:16:19 +0300

    add module

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 import DragManager from './DragManager';
+import DragAvatar from './DragAvatar';
+import DropTarget from './DropTarget';
+import DragZone from './DragZone';
 
 import TreeDragZone from './TreeDragZone';
 import TreeDropTarget from './TreeDropTarget';
+import TreeDragAvatar from './TreeDragAvatar';
 
-let tree = document.querySelector('#tree');
-let dragElement = document.querySelector('.draggable');
+const dnd = {
+    DragManager,
+    DragAvatar,
+    DropTarget,
+    DragZone,
+    TreeDragZone,
+    TreeDropTarget,
+    TreeDragAvatar
+};
 
-const treeDragZone = new TreeDragZone(tree);
-const treeDropTarget = new TreeDropTarget(tree);
+export default dnd;
+
+// let tree = document.querySelector('#tree');
+// let dragElement = document.querySelector('.draggable');
+//
+// const treeDragZone = new TreeDragZone(tree);
+// const treeDropTarget = new TreeDropTarget(tree);
 
 
