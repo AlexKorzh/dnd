921e766867955c6792de04aaa70c144ef0092b6a
Alex <korzhlesha@gmail.com>
2018-07-07 17:20:37 +0300

    add files

--- dist/index.js
+++ dist/index.js
@@ -10,10 +10,10 @@ import TreeDragAvatar from './TreeDragAvatar';
 
 export { DragManager, DragAvatar, DropTarget, DragZone, TreeDragZone, TreeDropTarget, TreeDragAvatar, utils };
 
-let dragManager = DragManager.getInstance();
-
-let tree = document.querySelector('#tree');
-let dragElement = document.querySelector('.draggable');
-debugger;
-const treeDragZone = new TreeDragZone({ elem: tree });
-const treeDropTarget = new TreeDropTarget({ elem: tree });
+// let dragManager = DragManager.getInstance();
+//
+// let tree = document.querySelector('#tree');
+// let dragElement = document.querySelector('.draggable');
+//
+// const treeDragZone = new TreeDragZone({elem: tree});
+// const treeDropTarget = new TreeDropTarget({elem: tree});
